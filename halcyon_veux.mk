33ebaa392937ebb5aee0a7ce18394c90b214bca9
Isaiah <lqlp0011@gmail.com>
2024-04-08 09:32:13 +0800

    Welcome, Halcyon! (2/2)

--- halcyon_veux.mk
+++ halcyon_veux.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Halcyon stuff.
+$(call inherit-product, vendor/halcyon/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
